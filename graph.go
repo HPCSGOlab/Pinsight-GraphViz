bfcdf0354693be33f00714ac0590147e7e18c1b2
eliJI <elijahingr@gmail.com>
2023-12-06 15:02:24 -0500

    added files

---
+++ graph.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Node struct {
+    correllationId int
+    address string
+    size int
+    nodeType string
+}
+
+type Graph struct {
+
+}
+
+func (g *Graph) add(n Node) {
+
+}
+
+func (g * Graph) addEdge(n1 Node, n2 Node) {
+
+}
+
+//imports data from file as a string array
+func importData() []string {
+    
+}
+
+//extracts only relevant information from data
+func extractTraces(data []string) []string {
+
+}
+
+func main() {
+    fmt.Println("test")
+}
